a0d577ef395a8270bf496dd9fedf1dda625d9c59
Zhaohui L <yunduxiaocheng@gmail.com>
2022-05-07 13:30:21 -0700

    Initial Commit

---
+++ Shared/FirstXcodeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstXcodeProjectApp.swift
+//  Shared
+//
+//  Created by Zhaohui Liu on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstXcodeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
